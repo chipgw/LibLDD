ebd22c8c2526f64cd1a293fd065b1c72c16f3354
Bart van Blokland <bartvblokl@gmail.com>
2014-04-12 14:16:58 +0200

    Ensured that the material loader allows byte[] as input

--- LibLDD/src/lib/ldd/materials/MaterialLoader.java
+++ LibLDD/src/lib/ldd/materials/MaterialLoader.java
@@ -1,6 +1,8 @@
 package lib.ldd.materials;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.HashMap;
 
@@ -12,13 +14,29 @@ import nu.xom.ParsingException;
 import nu.xom.ValidityException;
 
 public class MaterialLoader {
-
-	public static HashMap<Integer, Material> loadMaterials(File materialSrc) throws ValidityException, ParsingException, IOException {
+	
+	public static HashMap<Integer, Material> loadMaterials(File materialSrc) throws IOException {
 		if(!materialSrc.exists()) {
 			throw new RuntimeException("Could not find materials file. Did you unpack the Assets.lif file?");
 		}
+		int fileLength = (int) materialSrc.length();
+		byte[] fileContents = new byte[fileLength];
+		FileInputStream stream = new FileInputStream(materialSrc);
+		stream.read(fileContents);
+		stream.close();
+		return loadMaterials(fileContents);
+	}
+
+	public static HashMap<Integer, Material> loadMaterials(byte[] fileContents) throws IOException {
 		Builder builder = new Builder();
-		Document doc = builder.build(materialSrc);
+		Document doc;
+		try {
+			doc = builder.build(new ByteArrayInputStream(fileContents));
+		} catch (ValidityException e) {
+			throw new RuntimeException("Failed to parse Materials file: invalid XML. " + e.getMessage());
+		} catch (ParsingException e) {
+			throw new RuntimeException("Failed to parse Materials file: invalid XML. " + e.getMessage());
+		}
 		Element rootElement = doc.getRootElement();
 		HashMap<Integer, Material> materials = new HashMap<Integer, Material>();
 		Elements materialElements = rootElement.getChildElements();
@@ -28,7 +46,7 @@ public class MaterialLoader {
 		}
 		return materials;
 	}
-
+	
 	private static void readMaterial(HashMap<Integer, Material> materials, Element materialElement) {
 		int materialID = Integer.parseInt(materialElement.getAttributeValue("MatID"));
 		int red = Integer.parseInt(materialElement.getAttributeValue("Red"));
